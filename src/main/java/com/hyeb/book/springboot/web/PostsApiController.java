bd2f5acaf65dba3f499dfee252cf991ba3ab1dab
HYEBIN,PARK <hyeqls3742@gmail.com>
2021-09-17 23:22:13 +0900

    post update, response

---
+++ src/main/java/com/hyeb/book/springboot/web/PostsApiController.java
@@ -0,0 +1,31 @@
+package com.hyeb.book.springboot.web;
+
+
+import com.hyeb.book.springboot.service.posts.PostsService;
+import com.hyeb.book.springboot.web.dto.PostsResponseDto;
+import com.hyeb.book.springboot.web.dto.PostsSaveRequestDto;
+import com.hyeb.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService poststService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return poststService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return poststService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return poststService.findById(id);
+    }
+}
