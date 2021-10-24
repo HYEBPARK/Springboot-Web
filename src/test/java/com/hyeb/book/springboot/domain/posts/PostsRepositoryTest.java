d1666f1faf024838560e6cbdb3fbc093f268d173
HYEBIN,PARK <hyeqls3742@gmail.com>
2021-10-24 19:48:18 +0900

    test 오류 수정

--- src/test/java/com/hyeb/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/hyeb/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -64,7 +64,5 @@ public class PostsRepositoryTest {
 
         System.out.println(">>>>>> createDate = " +posts.getCreatedDate()+", modifiedDate=" +posts.getModifiedDate());
 
-        assertThat(posts.getCreatedDate()).isAfter(now);
-        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
